fc23dd451caa259b12bb9720754b3ce375790921
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-03-01 13:47:44 +0600

    bug fix

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const searchBtn = document.getElementById('search');
 const searchBox = document.getElementById('searchBox');
 const resultBox = document.getElementById('resultContainer');
-
+let searchText = '';
 
 const callApi = () =>{
-    // const searchText = searchBox.value;
-    const searchText = 'iphone';
+    searchText = searchBox.value;
+    // const searchText = 'iphone';
     let phoneList = fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
                         .then(res => res.json())
                         .then(data =>printResult(data.data));
 }
 const printResult = result => {
     console.log(result);
-    result.forEach(item => {
+    resultBox.innerHTML = '';
+    searchBox.value = '';
+    const h4 = document.createElement('h4');
+    h4.innerHTML = `Here is your search result for "${searchText}"`;
+    resultBox.appendChild(h4);
+    let count = 0;
+    for(const item of result) {
+        if(count > 19){
+            break;
+        }
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
         div.classList.add('col-12');
@@ -22,11 +31,14 @@ const printResult = result => {
             <div class="card-body">
                 <h5 class="card-title">${item.phone_name}</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Details</a>
             </div>
         </div>
         `;
         resultBox.appendChild(div);
-    });
+        count++;
+        
+        
+    };
 }
 searchBtn.addEventListener('click', callApi);
