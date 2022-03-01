d228aa2130970c809dfcc625d1a1e8ca62b01505
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-03-01 20:02:16 +0600

    CODE DRY

--- main.js
+++ main.js
@@ -1,35 +1,77 @@
+// Initializing variables with elements from HTML
+
 const searchBtn = document.getElementById('search');
 const searchBox = document.getElementById('searchBox');
+const spinner = document.getElementById('spinnerContainer');
 const resultBox = document.getElementById('resultContainer');
+
+
 let searchText = '';
 
-const callApi = () =>{
-    searchText = searchBox.value;
-    // const searchText = 'iphone';
-    let phoneList = fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-                        .then(res => res.json())
-                        .then(data =>printResult(data.data));
-}
-const printResult = result => {
-    console.log(result);
+/**
+ * Function for clearing Result Container
+ */
+const clearResultBox = () =>{
     resultBox.innerHTML = '';
-    searchBox.value = '';
+}
+
+
+/**
+ * Function for calling Search API
+ */
+const callSearchApi = () => {
+    searchText = searchBox.value;
+
+    //If user gives empty value for search
+    if (searchText != '') {
+        let phoneList = fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+            .then(res => res.json())
+            .then(data => printResult(data.data));
+        spinner.classList.toggle('d-none');
+        clearResultBox();
+    } else {
+        resultBox.innerHTML = '';
+        const h4 = document.createElement('h4');
+        h4.classList.add('text-center');
+        h4.classList.add('text-danger');
+        h4.innerHTML = `Please enter phone name`;
+        clearResultBox();
+    }
+}
+
+/**
+ * Function for printing result inside HTML
+ */
+const printResult = result => {
     const h4 = document.createElement('h4');
     h4.innerHTML = `Here is your search result for "${searchText}"`;
+
+    //If no phone found
+    if (result.length == 0) {
+        h4.innerHTML = `No phone found`;
+        resultBox.appendChild(h4);
+        spinner.classList.toggle('d-none');
+        return;
+    }
+    console.log(result);
+    searchBox.value = '';
+
     resultBox.appendChild(h4);
     let count = 0;
-    for(const item of result) {
-        if(count >= 20){
+    for (const item of result) {
+        if (count >= 20) {
             const btn = document.createElement('button');
             btn.classList.add('btn');
             btn.classList.add('btn-success');
             btn.classList.add('mt-5');
-            btn.setAttribute('id','showMore')
+            btn.setAttribute('id', 'showMore')
             btn.innerHTML = 'Show All'
             resultBox.appendChild(btn);
             break;
 
         }
+
+        //Single column containing single item from search response
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
         div.classList.add('col-12');
@@ -46,5 +88,6 @@ const printResult = result => {
         resultBox.appendChild(div);
         count++;
     };
+    spinner.classList.toggle('d-none');
 }
-searchBtn.addEventListener('click', callApi);
+searchBtn.addEventListener('click', callSearchApi);
